24f0ae75c850bb1a5484a37fac1f065590373abf
Viivi Puuronen <viivi.puuronen@hotmail.com>
2020-06-04 12:50:28 +0300

    alustettu minimax

--- TicTacToe/src/main/java/controls/AI.java
+++ TicTacToe/src/main/java/controls/AI.java
@@ -1,11 +1,37 @@
 package controls;
 
-
-
+import domain.*;
 /**
  * This class contains minimax and alpha-beta pruning for the game
  * @author vpuurone
  */
 public class AI {
+    
+    public static Cell player = Cell.CIRCLE;
+    public static Cell ai = Cell.CROSS;
+    
+    public static int[] bestMove(Board board) {
+        int[] bestMove = null;
+        int[][] possibleMoves = board.getPossibleMoves();
+        
+        return bestMove;
+    }
+    
+    public static int minimax(Board board) {
+        Cell winner = board.checkWinner();
+        int points;
+        if (winner != null) { 
+            if (winner == player) {
+                points = -1;
+                return points;
+            } else if (winner == Cell.BLANK) {
+                points = 0;
+                return points;
+            } else {
+                points = 1;
+                return points;
+            }
+        }
+    }
     
 }
